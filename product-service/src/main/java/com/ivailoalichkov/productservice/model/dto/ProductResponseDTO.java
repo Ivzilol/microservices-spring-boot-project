075591ad93bc74d00c12aec1105c2789f901c91c
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-10 19:38:59 +0300

    Refactor

--- product-service/src/main/java/com/ivailoalichkov/productservice/model/dto/ProductResponseDTO.java
+++ product-service/src/main/java/com/ivailoalichkov/productservice/model/dto/ProductResponseDTO.java
@@ -15,6 +15,7 @@ import java.math.BigDecimal;
 public class ProductResponseDTO {
 
     private String id;
+
     private String name;
     private String description;
     private BigDecimal price;
