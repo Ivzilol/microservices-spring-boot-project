ee141e803f8f118d8aff1c7436b1904e1fcc9871
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-17 14:45:38 +0300

    Add Kafka dependency and properties

---
+++ order-service/src/main/java/com/ivayloalichkov/orderservice/event/OrderPlacedEvent.java
@@ -0,0 +1,14 @@
+package com.ivayloalichkov.orderservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+
+    private String orderNumber;
+
+}
--- order-service/src/main/java/com/ivayloalichkov/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ivayloalichkov/orderservice/service/OrderService.java
@@ -1,11 +1,13 @@
 package com.ivayloalichkov.orderservice.service;
 
+import com.ivayloalichkov.orderservice.event.OrderPlacedEvent;
 import com.ivayloalichkov.orderservice.model.dto.InventoryResponseDTO;
 import com.ivayloalichkov.orderservice.model.dto.OrderLineItemsDTO;
 import com.ivayloalichkov.orderservice.model.dto.OrderRequestDTO;
 import com.ivayloalichkov.orderservice.model.entity.Order;
 import com.ivayloalichkov.orderservice.model.entity.OrderLineItems;
 import com.ivayloalichkov.orderservice.repository.OrderRepository;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -24,10 +26,12 @@ public class OrderService {
 
     private final WebClient.Builder webClientBuilder;
 
-    public OrderService(OrderRepository orderRepository, WebClient.Builder webClientBuilder) {
-        this.orderRepository = orderRepository;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
+    public OrderService(OrderRepository orderRepository, WebClient.Builder webClientBuilder, KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate) {
+        this.orderRepository = orderRepository;
         this.webClientBuilder = webClientBuilder;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public String placeOrder(OrderRequestDTO orderRequestDTO) {
@@ -40,6 +44,7 @@ public class OrderService {
         boolean allProductsInStock = isAllProductsInStock(order);
         if (Boolean.TRUE.equals(allProductsInStock)) {
             this.orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return "Order Placed Successfully";
         } else {
             throw new IllegalArgumentException("Product in not available!");
