f7254d473b7594cf4623c610a7e708d9e7fc2ceb
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-20 18:03:13 +0300

    Refactor

--- inventory-service/src/main/java/com/ivailoalichkov/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/ivailoalichkov/inventoryservice/repository/InventoryRepository.java
@@ -10,6 +10,5 @@ import java.util.Optional;
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-
     List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
