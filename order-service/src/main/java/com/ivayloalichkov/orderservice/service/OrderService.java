f8b43f853deaf765123d0a7d1256fea27ef66fd2
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-25 15:52:21 +0300

    Refactor

--- order-service/src/main/java/com/ivayloalichkov/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ivayloalichkov/orderservice/service/OrderService.java
@@ -69,7 +69,6 @@ public class OrderService {
                 .allMatch(InventoryResponseDTO::getIsInStock);
     }
 
-
     private OrderLineItems mapToDto(OrderLineItemsDTO orderLineItemsDTO) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDTO.getPrice());
