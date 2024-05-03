0851fba6af539fea22259bbdb0206e5d93a532d3
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-05-03 14:36:17 +0300

    Refactor

--- order-service/src/main/java/com/ivayloalichkov/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ivayloalichkov/orderservice/service/OrderService.java
@@ -28,7 +28,6 @@ public class OrderService {
 
     private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
-
     public OrderService(OrderRepository orderRepository, WebClient.Builder webClientBuilder, KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate) {
         this.orderRepository = orderRepository;
         this.webClientBuilder = webClientBuilder;
